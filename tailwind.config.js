49d9c5f9e835474eee8eb72b67123f0918c151ea
Daniel Arch <raresgeo9@gmail.com>
2026-07-06 15:06:56 +0300

    fix: whitelist row bg classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 module.exports = {
-    purge: [
-        './resources/views/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+    purge: {
+        content: [
+            './resources/views/**/*.blade.php',
+            './resources/**/*.js',
+            './resources/**/*.vue',
+        ],
+        options: {
+            // Row colours come from the DB and are assembled at runtime
+            // (bg-{{ colour }}-700), so purge can't see them in source.
+            safelist: [
+                'bg-green-700',
+                'bg-pink-700',
+                'bg-purple-700',
+                'bg-blue-700',
+                'bg-indigo-700',
+                'bg-yellow-700',
+                'bg-red-700',
+            ],
+        },
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
